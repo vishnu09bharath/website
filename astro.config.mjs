ed29d44c2cb06dd061e7957f525670cd148d547c
Vishnu Bharath <mr.vishnubharath@gmail.com>
2026-06-12 22:04:14 -0400

    revert cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
-import react from "@astrojs/react";
-import keystatic from "@keystatic/astro";
 
 export default defineConfig({
   site: "https://vishnubharath.com",
   output: "static",
   devToolbar: { enabled: false },
-  integrations: [react(), keystatic()],
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
